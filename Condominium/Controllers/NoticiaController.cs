1697026a0c8734b3c25815f64a85c50852510bed
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-13 00:33:38 -0600

    news table list (test)

---
+++ Condominium/Controllers/NoticiaController.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Services;
+using Infraestructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Condominium.Controllers
+{
+    public class NoticiaController : Controller
+    {
+        // GET: Noticia
+        public ActionResult Index()
+        {
+            IEnumerable<NOTICIA> lista = null;
+            try
+            {
+                IServiceNoticia _ServiceNoticia = new ServiceNoticia();
+                lista = _ServiceNoticia.GetNoticias();
+            }
+            catch
+            {
+                throw;
+            }
+            return View(lista);
+        }
+    }
+}
---
+++ Infraestructure/Repository/RepositoryNoticia.cs
@@ -0,0 +1,30 @@
+﻿using Infraestructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository
+{
+    public class RepositoryNoticia : IRepositoryNoticia
+    {
+        public IEnumerable<NOTICIA> GetNoticias()
+        {
+            try
+            {
+                IEnumerable<NOTICIA> lista = null;
+                using(MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    lista = ctx.NOTICIAs.ToList<NOTICIA>();
+                }
+                return lista;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
