93e8cb1b6addd6fc36b9062c3072c42b6e7010ab
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-20 23:41:19 -0600

    ESTADOS

--- Condominium/Controllers/EstadosCuentaController.cs
+++ Condominium/Controllers/EstadosCuentaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infraestructure.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,20 @@ namespace Condominium.Controllers
         // GET: EstadosCuenta
         public ActionResult Index()
         {
-            
+            List<GESTION_DEUDA> list = null;
+            try
+            {
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    list = ctx.GESTION_DEUDA.ToList();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            return View(list);
         }
     }
 }
