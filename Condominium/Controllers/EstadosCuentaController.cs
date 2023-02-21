1ca2e562d5458a9d2b60a0b8a8dc9313cfcfdc18
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-20 23:13:45 -0600

    estados de cuenta coding

---
+++ Condominium/Controllers/EstadosCuentaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Condominium.Controllers
+{
+    public class EstadosCuentaController : Controller
+    {
+        // GET: EstadosCuenta
+        public ActionResult Index()
+        {
+            
+        }
+    }
+}
