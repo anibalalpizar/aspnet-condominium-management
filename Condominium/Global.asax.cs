69a87c049a2d4748b19a862f2fa20d355eb3761f
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-25 21:55:20 -0600

    log4net done

--- Condominium/Global.asax.cs
+++ Condominium/Global.asax.cs
@@ -16,6 +16,8 @@ namespace Condominium
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
