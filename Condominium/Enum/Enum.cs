06631f88d34db5153f5220c518f1752dae19bdac
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-03-19 21:20:52 -0600

    Roles

---
+++ Condominium/Enum/Enum.cs
@@ -0,0 +1,1 @@
+﻿public enum Roles { Administrador = 1, Residente = 2 }
---
+++ Condominium/Security/AutorizeView.cs
@@ -0,0 +1,29 @@
+﻿using Infraestructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Condominium.Security
+{
+    public class AutorizeView
+    {
+        public static bool IsUserInRole(string[] nombreRoles)
+        {
+            IEnumerable<Roles> allowedroles = nombreRoles.
+                Select(a => (Roles)Enum.Parse(typeof(Roles), a));
+            bool authorize = false;
+            var oUsuario = (USUARIO)HttpContext.Current.Session["User"];
+            if (oUsuario != null)
+            {
+                foreach (var rol in allowedroles)
+                {
+                    if ((int)rol == oUsuario.ID_TIPO_USUARIO)
+                        return true;
+                }
+            }
+            return authorize;
+        }
+    }
+}
