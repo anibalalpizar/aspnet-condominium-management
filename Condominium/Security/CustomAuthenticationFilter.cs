9a6fda00c29a1d73f6f8311e3313aeaee85780d7
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-03-19 22:44:36 -0600

    security classes

---
+++ Condominium/Security/CustomAuthenticationFilter.cs
@@ -0,0 +1,37 @@
+﻿using Infraestructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Filters;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Condominium.Security
+{
+    public class CustomAuthenticationFilter : ActionFilterAttribute, IAuthenticationFilter
+    {
+        //Método para autenticar la solicitud
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if ((USUARIO)filterContext.HttpContext.Session["User"] == null)
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult)
+            {
+                // Redirija al Controller Login
+                filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                {
+                 { "controller", "Login" },
+                 { "action", "Index" }
+                 });
+            }
+        }
+    }
+}
---
+++ Condominium/Security/CustomAuthorizeAttribute.cs
@@ -0,0 +1,48 @@
+﻿using Infraestructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Condominium.Security
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        //Roles permitidos
+        private readonly int[] allowedroles;
+        public CustomAuthorizeAttribute(params int[] roles)
+        {
+            //roles Obtiene los roles de usuario autorizados
+            //para acceder al controlador o al método de acción.
+            this.allowedroles = roles;
+        }
+        //Verificaciones de autorización personalizadas
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            var oUsuario = (USUARIO)httpContext.Session["User"];
+            if (oUsuario != null)
+            {
+                foreach (var rol in allowedroles)
+                {
+                    if (rol == oUsuario.IdRol)
+                        return true;
+                }
+            }
+            return authorize;
+        }
+        //Procesa solicitudes HTTP que fallan en la autorización.
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            // Si hubo un error redireccione a el siguiente Controller y View
+            filterContext.Result = new RedirectToRouteResult(
+            new RouteValueDictionary
+            {
+             { "controller", "Login" },
+            { "action", "UnAuthorized" }
+                        });
+        }
+    }
+}
