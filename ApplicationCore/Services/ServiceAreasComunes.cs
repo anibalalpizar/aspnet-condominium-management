83cc335579f8ecf2660abdf62d49156df02982ce
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-03-27 20:55:23 -0600

    area comunes index

---
+++ ApplicationCore/Services/ServiceAreasComunes.cs
@@ -0,0 +1,19 @@
+﻿using Infraestructure.Model;
+using Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class ServiceAreasComunes : IServiceAreasComunes
+    {
+        public IEnumerable<RESERVA_AREA_COMUN> GetAreasComunes()
+        {
+            IRepositoryReservaAreasComunes repository = new RepositoryReservaAreasComunes();
+            return repository.GetAreasComunes();
+        }
+    }
+}
---
+++ Infraestructure/Repository/RepositoryReservaAreasComunes.cs
@@ -0,0 +1,40 @@
+﻿using Infraestructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Infraestructure.Repository
+{
+    public class RepositoryReservaAreasComunes : IRepositoryReservaAreasComunes
+    {
+        public IEnumerable<RESERVA_AREA_COMUN> GetAreasComunes()
+        {
+            IEnumerable<RESERVA_AREA_COMUN> lista = null;
+            try
+            {
+                using(MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    lista = ctx.RESERVA_AREA_COMUN.Include(x => x.ESTADO_RESERVACION).Include(x => x.AREA_COMUN).ToList();
+                }
+                return lista;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+    }
+}
